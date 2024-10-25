231a7e7dce3985f5191f929bb8fe6958d4e13c09
Pablilloz <pdelaflor2569@ceuandalucia.es>
2024-10-25 16:46:34 +0200

    proyecto.comit

---
+++ Proyecto3_3/src/Clase3_3.java
@@ -0,0 +1,9 @@
+package ;
+public class Clase3_3 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("Practica 3_3 " + "Pablo");
+	}
+
+}
---
+++ Proyecto3_3/src/Clase3_3a.java
@@ -0,0 +1,10 @@
+package clase3_3a;
+
+public class Clase3_3a {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("Canela en rama");
+	}
+
+}
